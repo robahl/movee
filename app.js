f9c13d5b7c88869128e31da2e2e4b10a76aef4ee
Robert Ahlberg <rob.ahlberg@gmail.com>
2017-05-13 20:33:09 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const logger = require('morgan')
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 3000;
+
+app.enable('trust proxy');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(logger('combined'));
+} else {
+  app.use(logger('dev'));
+}
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(port);
+
+io.on('connection', function (socket) {
+  console.console.log('Client connection');
+});
